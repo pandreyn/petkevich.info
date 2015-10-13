39a6087cf55f7af137cc7da41ac056d638cfcc68
Andrey Petkevich <pandreyn@gmail.com>
2015-10-13 17:53:27 +0300

    try heroku

--- start.js
+++ start.js
@@ -4,7 +4,7 @@ var config = require('./gulp/config');
 var debug = require('debug')('appServer');
 var app = require('./server_app.js');
 
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function () {
   debug('Express server listening on port ' + server.address().port);
